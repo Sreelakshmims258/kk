1b7123004f6a2d6b031a2f2ba508e6a6ec22b6fe
srxx-123 <140908429+srxx-123@users.noreply.github.com>
2023-09-14 16:23:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+const loginButton = document.querySelector('.btn-primary');
+
+// Add a click event listener to the button
+loginButton.addEventListener('click', function () {
+  // This function will run when the button is clicked
+  alert('Button clicked!');
+  // You can add your custom JavaScript logic here
+});
